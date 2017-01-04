675b0ccc9e24ebf3e967f40b6283cd13bfe5029c
Rahul Maurya <rahulmaurya.mnnit@gmail.com>
2017-01-04 13:33:57 +0530

    rendering todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,31 @@
 var React = require('react');
+var TodoList = require('TodoList');
+var Todo = require('Todo');
 
 var TodoApp = React.createClass({
+  getInitialState:function () {
+    return {
+      todos:[
+      {
+        id:1,text:"Walk the Dog"
+      },
+      {
+        id:2,text:"Clean the Yard"
+      },
+      {
+        id:3,text:"Kill the dog"
+      },
+      {
+        id:4,text:"Suicide"
+      }
+      ]
+    };
+  },
   render : function() {
+    var {todos} = this.state;
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos = {todos} />
       </div>
     );
   }
